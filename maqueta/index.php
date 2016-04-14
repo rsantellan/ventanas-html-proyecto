09ff2a9bbc9e1c4d8dbbaa14cf118b6d93a37534
Rodrigo Santellan <rsantellan@gmail.com>
2016-04-14 11:06:24 -0300

    Changes of index.

--- maqueta/index.php
+++ maqueta/index.php
@@ -1,160 +1,115 @@
-<!DOCTYPE html>
-<html lang="es">
-<?php $pagina = 'inicio';?>
-<?php include('_head.php');?>
-<body class="home">
-<?php include('_header.php');?>
-	<section class="container">
-		<div class="pageHeader" style="background-image: url(images/content/home.jpg);">
-		</div>
-		<div class="homeGrid">
-			<div class="mainItem clear"><!-- mainItem mainItemRight clear  - Reverse  -->
-				<div class="mainItemImg">
-					<img src="images/content/empresa-home.jpg" alt="">
-				</div>
-				<div class="mainItemDesc">
-					<h3>sobre nuestra </br>empresa</h3>
-					<p>Ventanal nace en el año 1978 y a lo largo de las décadas hemos adquirido experiencia en la fabricación y venta de aberturas de aluminio.
-					</br>Nuestros productos son utilizados en una gran variedad de construcciones. Edificios de vivienda, locales comerciales, viviendas unifamiliares, tanto reforma como obra nueva, donde puertas, ventanas, mamparas y estructuras de aluminio son la mejor opción por sus propiedades químicas y el bajo mantenimiento.</br>
-					Satisfacer la necesidad de cada cliente y cumplir con los tiempos pautados es para nosotros un compromiso .</p>
-					<a href="nosotros.php">
-					<span class="viewMore">ver más 
-							<i class="viewMore">
-								<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="9px" height="15px" viewBox="0 0 9 15" enable-background="new 0 0 9 15" xml:space="preserve">
-									<path fill="#96e9d5" d="M10-184.5l-0.826 0.757L1.826-177L1-177.758l7.349-6.742L1-191.243L1.826-192l7.349 6.743L10-184.5z M9.174-183.743L9.174-183.743L10-184.5L9.174-183.743z M9.175-185.257L10-184.5v0L9.175-185.257z"/>
-									<path fill="#96e9d5" d="M9 7.5L8.174 8.257L0.826 15L0 14.242L7.349 7.5L0 0.757L0.826 0l7.349 6.743L9 7.5z M8.174 8.3 L8.174 8.257L9 7.5L8.174 8.257z M8.175 6.743L9 7.5v0L8.175 6.743z"/>
-								</svg>
-							</i>
-						</span>
-					</a>
-				</div>
-			</div>
-			<div class="gridItemWrap gridItemWhite clear">
-				<a href="parasoles.php" class="gridItem clear">
-					<div class="gridItemImg">
-						<img src="images/content/home1.jpg" alt="">	
-					</div>
-					<div class="gridItemDesc">
-						<h3>sistemas de control solar</h3>
-						<p>En vistas de la necesidad de mejorar el coeficiente de Factor Solar (FS) de forma tal de disminuir la irradiación solar incidente que entra al local a través de elementos vidriados. </p>
-						<span class="viewMore">ver más 
-							<i>
-								<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="9px" height="15px" viewBox="0 0 9 15" enable-background="new 0 0 9 15" xml:space="preserve">
-									<path fill="#96e9d5" d="M10-184.5l-0.826 0.757L1.826-177L1-177.758l7.349-6.742L1-191.243L1.826-192l7.349 6.743L10-184.5z M9.174-183.743L9.174-183.743L10-184.5L9.174-183.743z M9.175-185.257L10-184.5v0L9.175-185.257z"/>
-									<path fill="#96e9d5" d="M9 7.5L8.174 8.257L0.826 15L0 14.242L7.349 7.5L0 0.757L0.826 0l7.349 6.743L9 7.5z M8.174 8.3 L8.174 8.257L9 7.5L8.174 8.257z M8.175 6.743L9 7.5v0L8.175 6.743z"/>
-								</svg>
-							</i>
-						</span>
-					</div>
-				</a>
-				<a href="monoblockN.php" class="gridItem gridItemWhite clear">
-					<div class="gridItemImg">
-						<img src="images/content/home2.jpg" alt="">	
-					</div>
-					<div class="gridItemDesc">
-						<h3>MONOBLOCK N</h3>
-						<p>Control solar y lumínico. La función principal de las cortinas es controlar la entrada del sol y de luz. 
-						Aislamiento acústico. Reduce sensiblemente el ruido exterior. </p>
-						<span class="viewMore">ver más 
-							<i>
-								<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="9px" height="15px" viewBox="0 0 9 15" enable-background="new 0 0 9 15" xml:space="preserve">
-								<path fill="#96e9d5" d="M10-184.5l-0.826 0.757L1.826-177L1-177.758l7.349-6.742L1-191.243L1.826-192l7.349 6.743L10-184.5z M9.174-183.743L9.174-183.743L10-184.5L9.174-183.743z M9.175-185.257L10-184.5v0L9.175-185.257z"/>
-								<path fill="#96e9d5" d="M9 7.5L8.174 8.257L0.826 15L0 14.242L7.349 7.5L0 0.757L0.826 0l7.349 6.743L9 7.5z M8.174 8.3 L8.174 8.257L9 7.5L8.174 8.257z M8.175 6.743L9 7.5v0L8.175 6.743z"/>
-								</svg>
-							</i>
-						</span>
-					</div>
-				</a>
-				<div class="gridItemWrapLeft">
-					<a href="#" class="gridItemVideo clear" style="cursor:default;">
-						<div class="gridItemImg">
-							<img src="images/content/home3.jpg" alt="">	
-						</div>
-					</a>
-					<a href="mamparas.php" class="gridItem gridItemWhite clear">
-						<div class="gridItemImg">
-							<img src="images/content/home4.jpg" alt="">	
-						</div>
-						<div class="gridItemDesc">
-							<h3>Mamparas</h3>
-							<p>Mamparas cristal 8mm templado corredizas batientes fijas. Aberturas con cristal simple o cristal doble ,segun las necesidades del cliente</p>
-							<span class="viewMore">ver más 
-								<i>
-									<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="9px" height="15px" viewBox="0 0 9 15" enable-background="new 0 0 9 15" xml:space="preserve">
-									<path fill="#96e9d5" d="M10-184.5l-0.826 0.757L1.826-177L1-177.758l7.349-6.742L1-191.243L1.826-192l7.349 6.743L10-184.5z M9.174-183.743L9.174-183.743L10-184.5L9.174-183.743z M9.175-185.257L10-184.5v0L9.175-185.257z"/>
-									<path fill="#96e9d5" d="M9 7.5L8.174 8.257L0.826 15L0 14.242L7.349 7.5L0 0.757L0.826 0l7.349 6.743L9 7.5z M8.174 8.3 L8.174 8.257L9 7.5L8.174 8.257z M8.175 6.743L9 7.5v0L8.175 6.743z"/>
-									</svg>
-								</i>
-							</span>
-						</div>
-					</a>
-				</div>
-				<div class="gridItemWrapRight">
-					<a href="rollers.php" class="gridItem2 clear">
-						<img src="images/content/home5.jpg" alt="">
-						<div class="gridItemDesc">
-							<h3>cortinas roller screen</h3>
-							<p>Con la intención de satisfacer nuevas  necesidades a nuestros clientes hemos incorporado a nuesta linea de productos ,las cortinas Roller Scrin y Black Out</p>
-							<span class="viewMore">ver más 
-								<i>
-									<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="9px" height="15px" viewBox="0 0 9 15" enable-background="new 0 0 9 15" xml:space="preserve">
-									<path fill="#96e9d5" d="M10-184.5l-0.826 0.757L1.826-177L1-177.758l7.349-6.742L1-191.243L1.826-192l7.349 6.743L10-184.5z M9.174-183.743L9.174-183.743L10-184.5L9.174-183.743z M9.175-185.257L10-184.5v0L9.175-185.257z"/>
-									<path fill="#96e9d5" d="M9 7.5L8.174 8.257L0.826 15L0 14.242L7.349 7.5L0 0.757L0.826 0l7.349 6.743L9 7.5z M8.174 8.3 L8.174 8.257L9 7.5L8.174 8.257z M8.175 6.743L9 7.5v0L8.175 6.743z"/>
-									</svg>
-								</i>
-							</span>
-						</div>
-					</a>
-				</div>
-			</div>
-		</div>
-
-		<div class="blogPosts">
-			<div class="blockTitle">algunos de nuestros productos</div>
-			<div class="blogPostWrap">
-				<div class="postItem">
-					<h4><a href="placard.php">Frentes de Placard</a></h4>
-					<p>Sistema corredizo de uso residencial tanto para placares de dormitorio como para cocina.</br> Diferentes tipologías que permiten la realización de frentes
-						de placard de 2 o más hojas corredizas.</p>
-				</div>
-				<div class="postItem">
-					<h4><a href="summa-alzante.php">Summa Alzante</a></h4>
-					<p> Los perfiles diseñados siguiendo las tendencias arquitectónicas a nivel mundial permiten integrar el ambiente con el paisaje exterior. </br>Su altura de hasta 2.70 metros permite una visión exterior inigualable.</p>
-				</div>
-				<div class="postItem">
-					<h4><a href="gala.php">Sistema de aberturas Gala</a></h4>
-					<p>Sistema de aberturas de diseño europeo para doble vidrio con cámara de aire. </br>Una completa línea de perfiles de aristas redondeadas con aislamiento térmico y acústico comprobado.</p>
-				</div>
-			</div>
-		</div>
-		<div class="homeGrid">
-			<div class="mainItem clear"><!-- mainItem mainItemRight clear  - Reverse  -->
-				<div class="mainItemImg">
-					<img src="images/content/map-home.jpg" alt="">
-				</div>
-				<div class="mainItemDesc mainItemDesc-contacto">
-					<h3>contacto</h3>
-					<div class="contact-block">
-						<img src="/images/marker.png">
-						<h3>dirección</h3>
-						<span>Dr.A.Gallinal 1890</span>
-					</div>
-					<div class="contact-block">
-						<img src="/images/tel.png">
-						<h3>teléfono</h3>
-						<span>(+598) 2619 1877 | 2613 2152</span>
-					</div>
-					<div class="contact-block">
-						<img src="/images/mail.png">
-						<h3>mail</h3>
-						<span><a href="mailto:info@ventanal.com.uy">info@ventanal.com.uy</a></span>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-	
-<?php include('_footer.php');?>
-<?php include('_mobileMenu.php');?>
-</body>
+<!DOCTYPE html>
+<html lang="es">
+<?php $pagina = 'inicio';?>
+<?php include('_head.php');?>
+<body class="home">
+<?php include('_header.php');?>
+	<section class="container">
+		<div class="pageHeader" style="background-image: url(images/content/home.jpg);">
+		</div>
+		<div class="homeGrid">
+			<div class="mainItem clear"><!-- mainItem mainItemRight clear  - Reverse  -->
+				<div class="mainItemImg">
+					<img src="images/content/empresa-home.jpg" alt="">
+				</div>
+				<div class="mainItemDesc">
+					<h3>sobre nuestra </br>empresa</h3>
+					<p>Ventanal nace en el año 1978 y a lo largo de las décadas hemos adquirido experiencia en la fabricación y venta de aberturas de aluminio.
+					</br>Nuestros productos son utilizados en una gran variedad de construcciones. Edificios de vivienda, locales comerciales, viviendas unifamiliares, tanto reforma como obra nueva, donde puertas, ventanas, mamparas y estructuras de aluminio son la mejor opción por sus propiedades químicas y el bajo mantenimiento.</br>
+					Satisfacer la necesidad de cada cliente y cumplir con los tiempos pautados es para nosotros un compromiso .</p>
+					<a href="nosotros.php">
+					<span class="viewMore">ver más 
+							<i class="viewMore">
+								<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="9px" height="15px" viewBox="0 0 9 15" enable-background="new 0 0 9 15" xml:space="preserve">
+									<path fill="#96e9d5" d="M10-184.5l-0.826 0.757L1.826-177L1-177.758l7.349-6.742L1-191.243L1.826-192l7.349 6.743L10-184.5z M9.174-183.743L9.174-183.743L10-184.5L9.174-183.743z M9.175-185.257L10-184.5v0L9.175-185.257z"/>
+									<path fill="#96e9d5" d="M9 7.5L8.174 8.257L0.826 15L0 14.242L7.349 7.5L0 0.757L0.826 0l7.349 6.743L9 7.5z M8.174 8.3 L8.174 8.257L9 7.5L8.174 8.257z M8.175 6.743L9 7.5v0L8.175 6.743z"/>
+								</svg>
+							</i>
+						</span>
+					</a>
+				</div>
+			</div>
+				<div class="gridItemWrapLeft">
+					<a href="#" class="gridItemVideo clear" style="cursor:default;">
+						<div class="gridItemImg">
+							<img src="images/content/home3.jpg" alt="">	
+						</div>
+					</a>
+				</div>
+				<div class="gridItemWrapRight">
+					<a href="#" class="gridItemVideo clear" style="cursor:default;">
+						<div class="gridItemImg">
+							<img src="images/content/home4.jpg" alt="">	
+						</div>
+					</a>
+				</div>			
+			<div class="gridItemWrap gridItemWhite clear">
+				<a href="parasoles.php" class="gridItem clear">
+					<div class="gridItemImg">
+						<img src="images/content/home5.jpg" alt="">	
+					</div>
+					<div class="gridItemImg">
+						<img src="images/content/home6.jpg" alt="">	
+					</div>
+				</a>
+				<a href="monoblockN.php" class="gridItem gridItemWhite clear">
+					<div class="gridItemImg">
+						<img src="images/content/home7.jpg" alt="">	
+					</div>
+					<div class="gridItemImg">
+						<img src="images/content/home2.jpg" alt="">	
+					</div>
+				</a>
+
+			</div>
+		</div>
+
+		<div class="blogPosts">
+			<div class="blockTitle">algunos de nuestros productos</div>
+			<div class="blogPostWrap">
+				<div class="postItem">
+					<h4><a href="placard.php">Frentes de Placard</a></h4>
+					<p>Sistema corredizo de uso residencial tanto para placares de dormitorio como para cocina.</br> Diferentes tipologías que permiten la realización de frentes
+						de placard de 2 o más hojas corredizas.</p>
+				</div>
+				<div class="postItem">
+					<h4><a href="summa-alzante.php">Summa Alzante</a></h4>
+					<p> Los perfiles diseñados siguiendo las tendencias arquitectónicas a nivel mundial permiten integrar el ambiente con el paisaje exterior. </br>Su altura de hasta 2.70 metros permite una visión exterior inigualable.</p>
+				</div>
+				<div class="postItem">
+					<h4><a href="gala.php">Sistema de aberturas Gala</a></h4>
+					<p>Sistema de aberturas de diseño europeo para doble vidrio con cámara de aire. </br>Una completa línea de perfiles de aristas redondeadas con aislamiento térmico y acústico comprobado.</p>
+				</div>
+			</div>
+		</div>
+		<div class="homeGrid">
+			<div class="mainItem clear"><!-- mainItem mainItemRight clear  - Reverse  -->
+				<div class="mainItemImg">
+					<img src="images/content/map-home.jpg" alt="">
+				</div>
+				<div class="mainItemDesc mainItemDesc-contacto">
+					<h3>contacto</h3>
+					<div class="contact-block">
+						<img src="/images/marker.png">
+						<h3>dirección</h3>
+						<span>Dr.A.Gallinal 1890</span>
+					</div>
+					<div class="contact-block">
+						<img src="/images/tel.png">
+						<h3>teléfono</h3>
+						<span>(+598) 2619 1877 | 2613 2152</span>
+					</div>
+					<div class="contact-block">
+						<img src="/images/mail.png">
+						<h3>mail</h3>
+						<span><a href="mailto:info@ventanal.com.uy">info@ventanal.com.uy</a></span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+<?php include('_footer.php');?>
+<?php include('_mobileMenu.php');?>
+</body>
 </html>
